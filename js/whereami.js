a91feb3ef8589d7ffd06babcecebd59b2db24672
michaeldimmitt <michaelgdimmitt@gmail.com>
2019-08-26 16:31:41 -0400

    cleanup

--- js/whereami.js
+++ js/whereami.js
@@ -15,16 +15,21 @@ function fetchJSONFile(path, callback) {
 // curl 'https://api.meetup.com/self/groups?page=20&access_token=c7343ae906e6ab44797193666e50fc1e&only=name' -H 'Accept: */*' -H 'Referer: http://127.0.0.1:3000/rsvp' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36' -H 'Sec-Fetch-Mode: cors' --compressed
 fetchJSONFile('https://api.meetup.com/self/events?page=1&scroll=next_upcoming&access_token=12900eeff1d07a0e05a3b9fdc06e35bc', function(data){
     console.log({data})
-    var nextEventDate = new Date(data[0].local_date);
+    
+    const { link, venue, local_date, name }  = data[0];
+
+    const address = (venue.address_1 + " " + venue.city + ", "  + venue.state);
+    const nextEventDate = new Date(local_date);
+    const name = name;
+
     const nextEventDayString = 
         new Intl.DateTimeFormat('en-US', {month: "short"}).format(nextEventDate) + ' ' +
         nextEventDate.getDate() 
-    var address = (data[0].venue.address_1 + " " + data[0].venue.city + ", "  + data[0].venue.state);
-    var link = data[0].link;
+    
     document.getElementById("whereami").innerHTML = `
     <span>
         Find me <a href=${link}>${nextEventDayString}</a> at 
-        ${data[0].name} 
+        ${name} 
         <br/>location: 
         <a href=${link}>
             ${address}
@@ -32,10 +37,5 @@ fetchJSONFile('https://api.meetup.com/self/events?page=1&scroll=next_upcoming&ac
         
     <span/>
     `
-    // <br/>
-    // <br/>
-    // ${nextEventDate}
-    // <br/>
-    // ${data[0].local_time}
 });
 
